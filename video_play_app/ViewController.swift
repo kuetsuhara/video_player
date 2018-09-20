80019a018f77ea135c0b75cd6f15756d7bc889ac
上津原 一利 [NEXT] <UetsuharaKazutoshi@next-group.jp>
2018-09-20 19:46:03 +0900

    first commit

---
+++ video_play_app/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  video_play_app
+//
+//  Created by 上津原 一利 [NEXT] on 2018/09/20.
+//  Copyright © 2018年 Kazutoshi Uetsuhara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.red // 背景を赤に
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ video_play_appTests/video_play_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  video_play_appTests.swift
+//  video_play_appTests
+//
+//  Created by 上津原 一利 [NEXT] on 2018/09/20.
+//  Copyright © 2018年 Kazutoshi Uetsuhara. All rights reserved.
+//
+
+import XCTest
+@testable import video_play_app
+
+class video_play_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
