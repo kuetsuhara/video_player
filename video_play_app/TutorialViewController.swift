b9e02c7864eb4da6ad11f06710019aef0e7975c7
上津原 一利 [NEXT] <UetsuharaKazutoshi@next-group.jp>
2018-09-25 17:20:26 +0900

    add paging view

--- video_play_app/TutorialViewController.swift
+++ video_play_app/TutorialViewController.swift
@@ -13,14 +13,8 @@ class TutorialViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var skipButton: UIButton!
     
-    var scrollScreenHeight:CGFloat!
-    var scrollScreenWidth:CGFloat!
-    
+
     let img:[String] = ["tutorial_0","tutorial_1","tutorial_2","tutorial_3"]
-    
-    var imageWidth:CGFloat!
-    var imageHeight:CGFloat!
-    var screenSize:CGRect!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,14 +22,29 @@ class TutorialViewController: UIViewController {
         // skip button action
         self.skipButton.addTarget(self, action: #selector(buttonEvent(sender:)), for: UIControlEvents.touchUpInside)
         
-        // scrollview setting
-        
-        screenSize = UIScreen.main.bounds
-        scrollScreenWidth = screenSize.width
-        
     }
     
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let scrollViewSize = self.scrollView.frame
+        let s_width = Int(scrollViewSize.size.width)
+        let s_height = Int(scrollViewSize.size.height)
+        
+        for num in 0 ... img.count{
+            print(num)
+            let imageView = UIImageView()
+            let xpoint = s_width * num
+            imageView.frame = CGRect(x: xpoint, y: 0, width: s_width - 10, height: s_height)
+            imageView.backgroundColor = UIColor.yellow
+            self.scrollView.addSubview(imageView)
+        }
+        
+        self.scrollView.contentSize = CGSize(width: s_width * img.count, height: s_height)
 
+    }
+    
     @objc func buttonEvent(sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
